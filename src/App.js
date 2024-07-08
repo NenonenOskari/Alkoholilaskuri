cdd6f12e9a47124c6f5a289007994748918562c5
NenonenOskari <nenonen.oskari@gmail.com>
2024-07-08 18:31:13 +0300

    Add existing project files to Git

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello beibe world!
+        </p>
+        <p>
+          Testiä pöytään, täältä testiä löytää
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
